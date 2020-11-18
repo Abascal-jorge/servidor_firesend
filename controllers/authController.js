fd4504b9c9064f4a98a9d7e725387bd1d6650fe3
Abascal <jorgeabascal20@gmail.com>
2020-11-18 16:42:10 -0500

    Cambios en el post auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,7 @@
 const Usuario = require("../models/Usuario");
 const bcrypt = require("bcrypt");
-exports.autenticarUsuario = async (req, res, next) => {
 
+exports.autenticarUsuario = async (req, res, next) => {
     //Revisar si hay errores
 
     //Buscar el usuario para ver si esta registrado 
